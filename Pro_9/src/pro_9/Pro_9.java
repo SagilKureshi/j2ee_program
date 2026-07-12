7a089317a58182a8fbab436e3997d04a45f0f8c1
Sagil Kureshi <sagilkureshi007@gmail.com>
2026-07-12 10:37:06 +0530

    Add JDBC practical programs 1 to 9

---
+++ Pro_9/src/pro_9/Pro_9.java
@@ -0,0 +1,41 @@
+package pro_9;
+
+import java.sql.*;
+
+public class Pro_9 {
+
+    public static void main(String[] args) {
+
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/company",
+                    "root",
+                    "root007"
+            );
+
+            System.out.println("Connection Successful...");
+
+            Statement st = con.createStatement();
+
+            ResultSet rs = st.executeQuery("SELECT * FROM emp ORDER BY empnm");
+
+            while (rs.next()) {
+
+                System.out.println(rs.getInt(1) + "\t"
+                        + rs.getString(2) + "\t"
+                        + rs.getString(3) + "\t"
+                        + rs.getString(4) + "\t"
+                        + rs.getDouble(5) + "\t"
+                        + rs.getString(6));
+            }
+
+            con.close();
+
+        } catch (Exception e) {
+            System.out.println("Error : " + e);
+        }
+    }
+}
